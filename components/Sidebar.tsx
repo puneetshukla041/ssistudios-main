ac8f8baf5aac27e7b765da4d2cae7ef85b0d3d37
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-09-03 11:48:25 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -379,6 +379,7 @@ export default function Sidebar({ forceActive, isOpen, toggleSidebar }: SidebarP
               shadow-md shadow-black/30 backdrop-blur-md
               transition-all cursor-pointer active:scale-[0.97]"
 >
+
   <svg
     xmlns="http://www.w3.org/2000/svg"
     className="h-4 w-4"
